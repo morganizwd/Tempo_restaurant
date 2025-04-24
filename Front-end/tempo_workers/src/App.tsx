138ee0f337f0750ce90e47fadac0926328ab054e
Leeyaoi <darya.ya2608@gmail.com>
2025-04-24 18:23:27 +0300

    added

--- Front-end/tempo_workers/src/App.tsx
+++ Front-end/tempo_workers/src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/loginPage/LoginPage";
 import MainAdminPage from "./pages/mainAdminPage/MainAdminPage";
 import MainWaiterPage from "./pages/mainWaiterPage/MainWaiterPage";
 import Header from "./modules/header/Header";
 import Footer from "./modules/footer/Footer";
+import { useGlobalStore } from "./shared/state/globalStore";
 
 const App = () => {
+  const { currentUser } = useGlobalStore();
   return (
     <div className="Container">
       <Header />
@@ -15,10 +17,18 @@ const App = () => {
           <Route path="/login" element={<LoginPage />} />
         </Routes>
         <Routes>
-          <Route path="/AdminPage" element={<MainAdminPage />} />
+          <Route path="/AdminPage" element={currentUser ? (
+            <MainAdminPage />
+          ) : (
+            <Navigate replace to={"/login"} />
+          )} />
         </Routes>
         <Routes>
-          <Route path="/WaiterPage" element={<MainWaiterPage />} />
+          <Route path="/WaiterPage" element={currentUser ? (
+            <MainWaiterPage />
+          ) : (
+            <Navigate replace to={"/login"} />
+          )} />
         </Routes>
       </BrowserRouter>
       <Footer />
